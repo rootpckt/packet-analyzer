e6e41b82da676b5664c2a3c10914a885644faf67
Pankaj Arya <pankajarya@MacBook-Air.local>
2026-02-15 18:31:58 +0530

    Add packet timestamp and structed output

--- pcktAnalyzer.cpp
+++ pcktAnalyzer.cpp
@@ -5,6 +5,9 @@
 #include <netinet/ip.h> // this struct represents to IPv4 Header
 #include <netinet/if_ether.h> // for Ethernet header definitions
 #include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <iomanip>
+#include <string>
 
 using namespace std;   // Allows direct use of standard library names (cout, endl) without std:: prefix
 
@@ -73,17 +76,30 @@ int main() {           // Entry point of the program; returns int status code to
     pcap_pkthdr* header;
     const u_char* packet;
 
-    while (true) {
+    int printed = 0;
+    const int MAX_PACKETS = 10;
+
+    cout << left << setw(5) << "No"
+         <<setw(10) << "Timestamp"
+         << setw(16) << "Source"
+         << setw(16) << "Destination"
+         << setw(8) << "Proto"
+         << setw(6) << "Len" << endl;
+
+    while (printed < MAX_PACKETS) {
         int res = pcap_next_ex(handle, &header, &packet);
 
         if (res == 1) {
-            cout << "Captured bytes: " << header->caplen << endl;
-            cout << "Original length: " << header->len << endl;
             // Parse Ethernet header
+
+            time_t packet_sec = header->ts.tv_sec;
+            tm* packet_time = localtime(&packet_sec);
+            char timebuf[9];
+            strftime(timebuf, sizeof(timebuf), "%H:%M:%S", packet_time);
             struct ether_header* eth = (struct ether_header*)packet;
             if (ntohs(eth->ether_type) != ETHERTYPE_IP) {
-                cout << "Non-IPv4 packet" << endl;
-                break;
+                // cout << "Non-IPv4 packet" << endl;
+                continue;
             }
             // Parse IPv4 header
             struct ip* iphdr = (struct ip*)(packet + sizeof(struct ether_header));
@@ -91,11 +107,29 @@ int main() {           // Entry point of the program; returns int status code to
             char dst_ip[INET_ADDRSTRLEN];
             inet_ntop(AF_INET, &iphdr->ip_src, src_ip, INET_ADDRSTRLEN);
             inet_ntop(AF_INET, &iphdr->ip_dst, dst_ip, INET_ADDRSTRLEN);
-            cout << "Source IP: " << src_ip << endl;
-            cout << "Destination IP: " << dst_ip << endl;
-            break;
+
+            string proto;
+            if ( iphdr->ip_p == IPPROTO_TCP ) {
+                proto = " TCP";
+            } else if ( iphdr->ip_p == IPPROTO_UDP ) {
+                proto = " UDP";
+            } else if ( iphdr->ip_p == IPPROTO_ICMP ) {
+                proto = " ICMP";
+            } else {
+                proto = " OTHER";
+            }
+
+            cout << left << setw(5) << printed + 1
+                 <<setw(10) << timebuf
+                 << setw(16) << src_ip
+                 << setw(16) << dst_ip
+                 << setw(8) << proto
+                 << setw(6) << header->len << endl;
+
+            printed++;
+
         } else if (res == 0) {
-            cout << "Waiting for packet..." << endl;
+            // Timeout: silently ignore and continue
         } else if (res == -1) {
             cerr << "Capture error: " << pcap_geterr(handle) << endl;
             break;
